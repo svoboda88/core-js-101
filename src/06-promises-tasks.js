edf2ff04f23cdf9383acacb60dc1c22876869847
Svoboda88 <zhenya.dowgun@gmail.com>
2022-07-25 16:22:17 +0300

    fix: solve linter errors

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -100,8 +100,8 @@ function getFastestPromise(array) {
  *    });
  *
  */
-function chainPromises(array, action) {
-  ;
+function chainPromises(/* array, action */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = {
